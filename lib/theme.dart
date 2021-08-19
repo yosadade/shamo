d5edb9020f62cb71ef3b255053bcfa2e45a2284e
yosadade <masyosad@gmail.com>
2021-08-19 14:40:19 +0700

    Slicing Sign In  pages

--- lib/theme.dart
+++ lib/theme.dart
@@ -10,6 +10,9 @@ Color backgroundColor2 = Color(0xff2B2937);
 Color backgroundColor3 = Color(0xff242231);
 Color primaryTextColor = Color(0xffF1F0F2);
 Color secondaryTextColor = Color(0xff999999);
+Color subtitleColor = Color(0xff504F5E);
+
+double defaultMargin = 30.0;
 
 TextStyle primaryTextStyle = GoogleFonts.poppins(
   color: primaryTextColor,
@@ -19,12 +22,20 @@ TextStyle secondaryTextStyle = GoogleFonts.poppins(
   color: secondaryTextColor,
 );
 
+TextStyle subtitleTextStyle = GoogleFonts.poppins(
+  color: subtitleColor,
+);
+
 TextStyle priceTextStyle = GoogleFonts.poppins(
   color: priceColor,
 );
 
+TextStyle purpleTextStyle = GoogleFonts.poppins(
+  color: primaryColor,
+);
+
 FontWeight light = FontWeight.w300;
-FontWeight reguler = FontWeight.w400;
+FontWeight regular = FontWeight.w400;
 FontWeight medium = FontWeight.w500;
 FontWeight semiBold = FontWeight.w600;
 FontWeight bold = FontWeight.w700;
