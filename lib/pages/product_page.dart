845eab5ea43c7229c26734396d86219181b6007b
yosadade <masyosad@gmail.com>
2021-08-23 21:13:22 +0700

    Product Page Bagian Product Slider

--- lib/pages/product_page.dart
+++ lib/pages/product_page.dart
@@ -1,10 +1,40 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:shamo/theme.dart';
 
-class ProductPage extends StatelessWidget {
+class ProductPage extends StatefulWidget {
+  @override
+  _ProductPageState createState() => _ProductPageState();
+}
+
+class _ProductPageState extends State<ProductPage> {
+  List images = [
+    'assets/image_shoes.png',
+    'assets/image_shoes.png',
+    'assets/image_shoes.png'
+  ];
+
+  int currentIndex = 0 ;
+
   @override
   Widget build(BuildContext context) {
+
+    Widget indicator(int index) {
+      return Container(
+        width: currentIndex == index ? 16 : 4,
+        height: 4,
+        margin: EdgeInsets.symmetric(horizontal: 2),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: currentIndex == index ? primaryColor : Color(0xffC4C4C4),
+        ),
+      );
+    }
+
     Widget header() {
+
+      int index = -1;
+
       return Column(
         children: [
           Container(
@@ -30,12 +60,38 @@ class ProductPage extends StatelessWidget {
               ],
             ),
           ),
+          CarouselSlider(
+            items: images
+                .map((e) => Image.asset(
+                      e,
+                      width: MediaQuery.of(context).size.width,
+                      height: 310,
+                      fit: BoxFit.cover,
+                    ))
+                .toList(),
+            options: CarouselOptions(
+              initialPage: 0,
+              onPageChanged: (index, reason){
+                setState(() {
+                  currentIndex = index;
+                });
+              }
+            ),
+          ),
+          SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: images.map((e) {
+              index++;
+              return indicator(index);
+            }).toList(),
+          )
         ],
       );
     }
 
     return Scaffold(
-      backgroundColor: primaryTextColor,
+      backgroundColor: Color(0xffECECEF),
       body: Center(
           child: ListView(
         children: [
