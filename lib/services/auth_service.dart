3895b9b0861a4f6f0c33ca062ca8c9fdbd02f30d
yosadade <masyosad@gmail.com>
2021-08-27 23:17:10 +0700

    Menambahkan Auth Service

---
+++ lib/services/auth_service.dart
@@ -0,0 +1,36 @@
+import 'dart:convert';
+
+import 'package:shamo/models/user_model.dart';
+import 'package:http/http.dart' as http;
+
+class AuthService {
+  String baseUrl = 'https://shamo-backend.buildwithangga.id/api';
+
+  // buat function
+  Future<UserModel> register(
+      {required String name,
+      required String username,
+      required String email,
+      required String password}) async {
+    var url = Uri.tryParse('$baseUrl/register');
+    var headers = {'content-type': 'application/json'};
+    var body = jsonEncode({
+      'name': name,
+      'username': username,
+      'email': email,
+      'password': password,
+    });
+
+    var response = await http.post(url!, headers: headers, body: body);
+
+    if (response.statusCode == 200) {
+      var data = jsonDecode(response.body)['data'];
+      UserModel user = UserModel.fromJson(data['data']);
+      user.token = 'Beared ' + data['access_token'];
+
+      return user;
+    } else {
+      throw Exception('Gagal register');
+    }
+  }
+}
